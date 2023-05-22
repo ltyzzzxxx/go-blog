09f813c2bb22c7d37697bedddf977c5f52157c8f
ltyzzz <ltyzzz2000@gmail.com>
2023-05-22 23:07:50 +0800

    feat: 实现配置方案

--- main.go
+++ main.go
@@ -1,15 +1,28 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"go-blog/bootstrap"
+	btsConfig "go-blog/config"
+	"go-blog/pkg/config"
 )
 
+func init() {
+	btsConfig.Initialize()
+}
+
 func main() {
+
+	var env string
+	flag.StringVar(&env, "env", "", "加载 .env 文件，如 --env=testing 加载的是 .env.testing 文件")
+	flag.Parse()
+	config.InitConfig(env)
+
 	router := gin.New()
 	bootstrap.SetupRoute(router)
-	err := router.Run(":3000")
+	err := router.Run(":" + config.Get("app.port"))
 	if err != nil {
 		fmt.Println(err.Error())
 	}
