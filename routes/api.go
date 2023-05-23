91990e4255e8ebee39825476a0448b3776b4ccdd
litanyu <litianyu.zzz@bytedance.com>
2023-05-23 21:22:28 +0800

    feat: email是否注册接口

--- routes/api.go
+++ routes/api.go
@@ -18,6 +18,7 @@ func RegisterAPIRoutes(r *gin.Engine) {
 		{
 			suc := new(auth.SignupController)
 			authGroup.POST("/signup/phone/exist", suc.IsPhoneExist)
+			authGroup.POST("/signup/email/exist", suc.IsEmailExist)
 		}
 	}
 }
