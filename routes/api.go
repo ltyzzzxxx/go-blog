43a7f814cee6e9d72748b10aac6bca1cef29f132
ltyzzz <ltyzzz2000@gmail.com>
2023-05-30 20:15:50 +0800

    feat: 密码登录&refresh token

--- routes/api.go
+++ routes/api.go
@@ -27,6 +27,8 @@ func RegisterAPIRoutes(r *gin.Engine) {
 			authGroup.POST("/verify-codes/email", vcc.SendUsingEmail)
 			lgc := new(auth.LoginController)
 			authGroup.POST("/login/using-phone", lgc.LoginByPhone)
+			authGroup.POST("/login/using-password", lgc.LoginByPassword)
+			authGroup.POST("/login/refresh-token", lgc.RefreshToken)
 		}
 	}
 }
